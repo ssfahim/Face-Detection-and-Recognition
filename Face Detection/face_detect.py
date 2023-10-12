4a3ed873d7094ff8b32682b61ba2aa5eb7758a2d
ssfahim <sohayeebsazid.fahim@gmail.com>
2023-10-11 23:41:44 -0230

    Create face_detect.py

---
+++ Face Detection/face_detect.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+img = cv.imread("singleFace.jpg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors=1)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+cv.imshow(f'Detected Faces & Number of faces found = {len(faces_rect)}', img)
+# cv.imshow('Detected Faces', img)
+cv.waitKey(0)
+
