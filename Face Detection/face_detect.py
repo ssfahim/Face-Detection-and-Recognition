6a61d28b96c9b3a6d275f5b02003905d82025eed
ssfahim <sohayeebsazid.fahim@gmail.com>
2023-10-12 00:26:10 -0230

    Update face_detect.py

--- Face Detection/face_detect.py
+++ Face Detection/face_detect.py
@@ -1,18 +1,23 @@
 import cv2 as cv
 
-img = cv.imread("singleFace.jpg")
+img = cv.imread("groupOf5.jpg")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-haar_cascade = cv.CascadeClassifier('haar_face.xml')
+# Verify the path to the Haar Cascade XML file
+cascade_path = 'haarcascade_frontalface_default.xml'
 
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor= 1.1, minNeighbors=1)
+# Load the Haar Cascade classifier
+haar_cascade = cv.CascadeClassifier(cascade_path)
 
-print(f'Number of faces found = {len(faces_rect)}')
+if haar_cascade.empty():
+    raise Exception(f"Error loading cascade classifier: {cascade_path}")
 
-for (x,y,w,h) in faces_rect:
-    cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), thickness=2)
+# Adjust the scaleFactor and minNeighbors values
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=1)
+
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
 
 cv.imshow(f'Detected Faces & Number of faces found = {len(faces_rect)}', img)
-# cv.imshow('Detected Faces', img)
 cv.waitKey(0)
-
