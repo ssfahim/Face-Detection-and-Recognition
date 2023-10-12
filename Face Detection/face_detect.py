e48b0b93ed1e5355c37101322be9162b7cafaccc
ssfahim <sohayeebsazid.fahim@gmail.com>
2023-10-12 00:39:17 -0230

    Update face_detect.py

--- Face Detection/face_detect.py
+++ Face Detection/face_detect.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img = cv.imread("groupOf3.jpg")
+img = cv.imread("5People.jpg")
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Verify the path to the Haar Cascade XML file
@@ -13,7 +13,7 @@ if haar_cascade.empty():
     raise Exception(f"Error loading cascade classifier: {cascade_path}")
 
 # Adjust the scaleFactor and minNeighbors values
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.8, minNeighbors=1)
 
 
 for (x, y, w, h) in faces_rect:
